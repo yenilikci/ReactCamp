ae99ba57aab6be6a74c570f68dbd178408c3c132
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-02 22:24:23 +0300

    var, let keyword kullanımı

---
+++ src/app.js
@@ -0,0 +1,22 @@
+//değişken tanımlarken kullandığımız keywordler
+//var, let, const
+var sayi1 = 10 //; opsiyonel
+//sayi1 = "String Deger"
+var sayi1 = "String Deger"
+let sayi2 = 10
+//let sayi2 = "String Deger"
+let sayi3 = 10
+sayi3 = "String deger"
+sayi3 = {id:1,name:'Engin'}
+student = {
+    id:1,
+    name:'Engin'
+} //student is not defined
+let student = {
+    id:1,
+    name:'Engin'
+}
+console.log(student)
+
+
+
