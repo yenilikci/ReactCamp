92a43e66e3b0ccf30f57df86b3447a6ce1b1ca23
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-06-06 08:29:12 +0300

    servisten gelen veriler ProductList içerisindeki tabloya yerleştirildi

--- 11)React Giriş/camp-project/src/services/productService.js
+++ 11)React Giriş/camp-project/src/services/productService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export default class productService {
+export default class ProductService {
   getProducts() {
     return axios.get("http://localhost:8080/api/products/getall")
   }
